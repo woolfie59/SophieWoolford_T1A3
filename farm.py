e437dbe62225e266d9fef18200ae38eb90fce932
woolfie59 <woolford.sophie@gmail.com>
2024-04-29 17:40:54 +1000

    Created repo and written basic app functionality

---
+++ farm.py
@@ -0,0 +1,12 @@
+# Welcome to your turnip farm
+# Please select one of the following options:
+# 1. Plant a seed
+# 2. Harvest a turnip
+# 3. View your turnips
+
+# Output
+# 1. You planted a seed
+# 2. You harvested a turnip
+# 3.1 You don't have any turnips. Plant and harvest turnips to view them here.
+# 3.2 Your turnips: 1 / You have 1 turnips
+# 4. (anything else) You goofball! Select 1 or 2 or 3 from the menu.
