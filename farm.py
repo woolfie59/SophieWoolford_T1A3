e1811a8bb76b2a71b3da9389ac4a9c80e4c67b89
woolfie59 <woolford.sophie@gmail.com>
2024-04-30 18:27:09 +1000

    Added functions page, and csv document

--- farm.py
+++ farm.py
@@ -11,15 +11,35 @@
 # 3.2 Your turnips: 1 / You have 1 turnips
 # 4. (anything else) You goofball! Select 1 or 2 or 3 from the menu.
 
+from farm_functions import plant_turnip, harvest_turnip, view_turnips
+
 print("Welcome to your turnip farm!")
 
 def create_menu():
-    print("Enter 1 to plant a turnip")
-    print("Enter 2 to harvest a turnip")
-    print("Enter 3 to view your turnips")
-    print("Enter 4 to leave your farm")
+    print("Enter 1 to plant a turnip.")
+    print("Enter 2 to harvest a turnip.")
+    print("Enter 3 to view your turnips.")
+    print("Enter 4 to leave your farm.")
 
     user_choice = input("Enter your selection: ")
     return user_choice
 
-print(create_menu())
+file_name = "list.csv"
+
+choice = ""
+
+while choice != "4":
+    choice = create_menu()
+
+    if (choice == "1"):
+        plant_turnip()
+    elif (choice == "2"):
+        harvest_turnip()
+    elif (choice == "3"):
+        view_turnips()
+    elif (choice == "4"):
+        print("You entered 4.")
+    else:
+        print("You goofball! Select 1 or 2 or 3 or 4 from the menu.")
+
+print("See ya later, farmer!")
---
+++ farm_functions.py
@@ -0,0 +1,8 @@
+def plant_turnip():
+    print("Plant a turnip")
+
+def harvest_turnip():
+    print("Harvest a turnip")
+
+def view_turnips():
+    print("View turnips")
