7896c0ff22bba5f88158dc80c25514110cdf0c19
woolfie59 <woolford.sophie@gmail.com>
2024-04-30 17:26:41 +1000

    Printed welcome message and main menu

--- farm.py
+++ farm.py
@@ -10,3 +10,16 @@
 # 3.1 You don't have any turnips. Plant and harvest turnips to view them here.
 # 3.2 Your turnips: 1 / You have 1 turnips
 # 4. (anything else) You goofball! Select 1 or 2 or 3 from the menu.
+
+print("Welcome to your turnip farm!")
+
+def create_menu():
+    print("Enter 1 to plant a turnip")
+    print("Enter 2 to harvest a turnip")
+    print("Enter 3 to view your turnips")
+    print("Enter 4 to leave your farm")
+
+    user_choice = input("Enter your selection: ")
+    return user_choice
+
+print(create_menu())
